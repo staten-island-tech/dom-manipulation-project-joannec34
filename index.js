dba28df0efc871a32a7863cc98c357799e1f091a
joannec34 <joannechen9058@gmail.com>
2022-11-07 14:00:26 -0500

    wqf wgeggseg

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-<<<<<<< HEAD
 //dom manipulation
 
 /* const button = document.getElementById("button"); //use when getting an id
 const text = document.querySelector("#text"); //use when getting a class
 console.log(button); */
 
-const domSelectors = {
+const DOMSelectors = {
   button: document.getElementById("button"),
-  box: document.getElementById("box"),
   text: document.querySelector("#text"),
-  point: document.querySelectorAll(".point"), //using .querySelectorAll creates a nodelist, which acts similar to an array
+  box: document.getElementById("box"),
+  points: document.querySelectorAll(".point"), //nodelist
 };
 
-console.log(domSelectors.point);
-=======
-//dom manipulation project
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "red";
+  text.textContent = "this is now a bigger red box";
+  text.style.fontSize = "40px";
+}
 
->>>>>>> parent of 8fea1b8 (y)
+DOMSelectors.button.addEventListener("click", function(){
+  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+});
+
+function changeLi() {
+  let pointIndex = 1;
+  DOMSelectors.points.forEach((point) => {
+  point.addEventListener("click", function(){
+    point.textContent = 'hello i am point ${pointIndex}';
+  });
+});
+}
+
+//fix pointIndex
+
+changeLi();
+
+/*
+Create an object called “DOMSelectors” to hold your DOM Selectors
+Create a function that creates an object and calls the following functions
+Create a function that injects the newly created object into the DOM
+Create a function that clears the input fields after injecting the object
+Create a function to remove an object after they have been created
+*/
+
+
+
