7c949f2c7fc5cab68da29391358b2a61f567680e
joannec34 <joannechen9058@gmail.com>
2022-11-09 23:29:24 -0500

    hfffh

--- index.js
+++ index.js
@@ -1,37 +1,5 @@
 //dom manipulation
 
-/* const button = document.getElementById("button"); //use when getting an id
-const text = document.querySelector("#text"); //use when getting a class
-console.log(button); */
-
-/* const DOMSelectors = {
-  button: document.getElementById("button"),
-  text: document.querySelector("#text"),
-  box: document.getElementById("box"),
-  points: document.querySelectorAll(".point"), //nodelist
-};
-
-function backgroundAndText(background, text) {
-  background.style.backgroundColor = "red";
-  text.textContent = "this is now a bigger red box";
-  text.style.fontSize = "40px";
-}
-
-DOMSelectors.button.addEventListener("click", function(){
-  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
-});
-
-function changeLi() {
-  let pointIndex = 1;
-  DOMSelectors.points.forEach((point) => {
-  point.addEventListener("click", function(){
-    point.textContent = `hello i am point ${pointIndex}`;
-    pointIndex++;
-  });
-});
-}
-changeLi(); */
-
 /*
 Create an object called “DOMSelectors” to hold your DOM Selectors
 Create a function that creates an object and calls the following functions
@@ -41,19 +9,20 @@ Create a function to remove an object after they have been created
 */
 
 const DOMSelectors = {
+  box: document.getElementById("box"),
   button: document.getElementById("btn"),
-  box: document.getElementById("container-box"),
-  input: document.querySelector(`#input`),
-}
+  input1: document.querySelector(`#input1`),
+  input2: document.querySelector(`#input2`),
+};
 
-const cat = "meow";
+console.log(DOMSelectors.box);
 
-DOMSelectors.box.insertAdjacentHTML("afterend",
-`<h1>we are a ${cat}</h1>`);
-
-DOMSelectors.button.addEventListener("click", function(){
-  let input = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML("beforeend",
-  `<p>${input}</p>`)
-  DOMSelectors.input.value = "";
-})
+DOMSelectors.button.addEventListener("click", function () {
+  let input1 = DOMSelectors.input1.value;
+  let input2 = DOMSelectors.input2.value;
+  let percentage = Math.floor(Math.random() * (100 - 1)) + 1;
+  DOMSelectors.box.insertAdjacentHTML(
+    "afterend",
+    `<p>${input1} and ${input2} are ${percentage}% compatibile</p>`
+  );
+});
