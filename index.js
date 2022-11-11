ca844e520d490e4b676a3a4ee7c10ccb659bda3d
joannec34 <joannechen9058@gmail.com>
2022-11-10 19:42:41 -0500

    fail

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ Create a function to remove an object after they have been created
 const DOMSelectors = {
   box1: document.getElementById("box1"),
   box2: document.getElementById("box2"),
-  button: document.getElementById("btn"),
+  form: document.getElementById("form"),
   input1: document.querySelector(`#input1`),
   input2: document.querySelector(`#input2`),
 };
 
-DOMSelectors.button.addEventListener("submit", function () {
+DOMSelectors.form.addEventListener("submit", function () {
   let input1 = DOMSelectors.input1.value;
   let input2 = DOMSelectors.input2.value;
   let percentage = Math.floor(Math.random() * (100 - 1)) + 1;
   DOMSelectors.box2.insertAdjacentHTML(
-    "beforeend",
+    "afterbegin",
     `<p>${input1} and ${input2} are ${percentage}% compatibile</p>`
   );
   if (percentage < 50) {
@@ -30,14 +30,11 @@ DOMSelectors.button.addEventListener("submit", function () {
       `<p>you guys are not meant for each other</p>
       <img src="https://www.clipartkey.com/mpngs/m/152-1524961_download-crying-emoji-face-iphone-ios-emojis-in.png" alt="bad">`
     );
-  }
-  else {
+  } else {
     DOMSelectors.box2.insertAdjacentHTML(
       "beforeend",
       `<p>you guys are meant for each other</p>
       <img src="https://www.kindpng.com/picc/m/34-345650_smiling-face-with-3-hearts-png-transparent-png.png" alt="good">`
     );
-  };
+  }
 });
-
-//submit thing doesnt work cuz of form element thing
