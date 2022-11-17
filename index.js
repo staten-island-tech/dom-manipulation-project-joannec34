019a0065d34fe181b44c01574c4e838d5e468c2d
joannec34 <joannechen9058@gmail.com>
2022-11-16 20:47:24 -0500

    hekellp

--- index.js
+++ index.js
@@ -9,64 +9,69 @@ Create a function to remove an object after they have been created
 */
 
 const DOMSelectors = {
-  box1: document.querySelector(".box1"),
+  body: document.querySelector("body"),
   box2: document.querySelector(".box2"),
-  box2stuff: document.querySelectorAll(".box2"),
   byeimage: document.getElementById("byeimage"),
   button: document.getElementById("btn"),
-  urname: document.querySelector(`#urname`),
-  theirname: document.querySelector(`#theirname`),
+  urname: document.getElementById("urname"),
+  theirname: document.getElementById("theirname"),
   form: document.getElementById("form"),
-  removebtn: document.getElementById("removebtn"),
+  removebtn: document.querySelector("#removebtn"),
+  heartbutton: document.getElementById("heart"),
 };
-
-console.log(DOMSelectors.box);
 
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
   let urname = DOMSelectors.urname.value;
   let theirname = DOMSelectors.theirname.value;
-  let luvpercentage = Math.floor(Math.random() * (100 - 1)) + 1;
+  //let luvpercentage = Math.floor(Math.random() * (100 - 1)) + 1;
+  let luvpercentage = 50;
   const byeimage = DOMSelectors.byeimage;
   byeimage.remove();
   DOMSelectors.box2.insertAdjacentHTML(
     "beforeend",
     `<p>${urname} and ${theirname} are ${luvpercentage}% compatibile.</p>`
   );
-  if (luvpercentage < 50) {
+  //25 50 75 100
+  if (luvpercentage < 25) {
     DOMSelectors.box2.insertAdjacentHTML(
       "beforeend",
       `<p class="displaytext">Maybe it's time to find someone new.</p>
-      <img src="https://media.tenor.com/5eBi-G3uqmoAAAAi/cute-crying.gif" alt="bad">`
+      <img src="https://media.tenor.com/5eBi-G3uqmoAAAAi/cute-crying.gif" alt="verybad">`
+    );
+  } else if (luvpercentage >= 25 && luvpercentage < 50) {
+    DOMSelectors.box2.insertAdjacentHTML(
+      "beforeend",
+      `<p class="displaytext">Maybe stay as friends?</p>
+      <img src="https://media.tenor.com/BnL0rOm6j3sAAAAi/quby-cute.gif" alt="bad">`
+    );
+  } else if (luvpercentage >= 50 && luvpercentage < 75) {
+    DOMSelectors.box2.insertAdjacentHTML(
+      "beforeend",
+      `<p class="displaytext">You guys go well together!</p>
+      <img src="https://media.tenor.com/VMBU8GtEvsIAAAAi/quby-cute.gif" alt="good">`
     );
   } else {
     DOMSelectors.box2.insertAdjacentHTML(
       "beforeend",
       `<p class="displaytext">You guys are meant for each other!</p>
-      <img src="https://media.tenor.com/-MlEF7aRcA0AAAAi/tkthao219-quby.gif" alt="good">`
+      <img src="https://media.tenor.com/0vTpvmaxXZ8AAAAi/line.gif" alt="verygood">`
     );
   }
   DOMSelectors.box2.insertAdjacentHTML(
     "beforeend",
-    `<p><button type="button" id="removebtn">Try Again</button></p>`
+    `<p><button id="removebtn">Try Again</button></p>`
   );
   function clearinput() {
     DOMSelectors.urname.value = "";
     DOMSelectors.theirname.value = "";
-  };
+  }
   clearinput();
-  });
+});
 
-    //FIX THE MESS BELOW SOMEHOW XD
-  DOMSelectors.removebtn.addEventListener("click", function() {
-    let remove = DOMSelectors.box2stuff;
-    remove.forEach((node) => {
-      node.addEventListener("click", function(event) {
-        event.target.parentElement.remove();
-      })
-    });
-    DOMSelectors.box2.insertAdjacentHTML(
-      "beforeend",
-      `<img id="byeimage" src="https://media.tenor.com/XsWhfNnJu5cAAAAi/soft-meme.gif" alt="heart">`
-    );
-  });
+DOMSelectors.heartbutton.addEventListener("click", function () {
+  function changetheme(background) {
+    background.style.backgroundColor = "#F1DCCD";
+  }
+  changetheme(DOMSelectors.body, DOMSelectors.text);
+});
